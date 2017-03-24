21568129c839b3d11c66aa85648e3898b7ce694e
ian logan <ian.logan@anm.com>
2017-03-24 13:25:46 -0700

    debug

--- service_nginx.sh
+++ service_nginx.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+exec > >(tee -a /usr/local/osmosix/logs/nginx_$$.log) 2>&1
+
+OSSVC_HOME=/usr/local/osmosix/service
+. /usr/local/osmosix/etc/.osmosix.sh
+. /usr/local/osmosix/etc/userenv
+# . $OSSVC_HOME/utils/cfgutil.sh
+# . $OSSVC_HOME/utils/install_util.sh
+# . $OSSVC_HOME/utils/os_info_util.sh
+# . $OSSVC_HOME/utils/agent_util.sh
+#
+
 majversion=$(lsb_release -rs | cut -f1 -d.)
 minversion=$(lsb_release -rs | cut -f2 -d.)
 
@@ -149,7 +160,7 @@ case $1 in
     ;;
   deploy)
     configureNginx
-    deployContent
+    # deployContent
     ;;
   upgrade)
     stopNginx
