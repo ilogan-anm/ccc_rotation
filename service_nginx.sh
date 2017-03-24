7c7fbd752bbdef7275394876ae85420b53f704b9
ian logan <ian.logan@anm.com>
2017-03-24 13:14:13 -0700

    add deploy again

--- service_nginx.sh
+++ service_nginx.sh
@@ -70,6 +70,64 @@ stopNginx ()
   fi
 }
 
+configureNginx ()
+{
+  sed -i s/^/\#/ /etc/nginx/conf.d/default.conf
+  cat << FOE > /etc/nginx/conf.d/main.conf
+  server {
+      listen       80;
+      server_name  _;
+
+      #charset koi8-r;
+      #access_log  /var/log/nginx/log/host.access.log  main;
+
+      location / {
+          root   /var/www/html;
+          index  index.html index.htm;
+      }
+
+      #error_page  404              /404.html;
+
+      # redirect server error pages to the static page /50x.html
+      #
+      error_page   500 502 503 504  /50x.html;
+      location = /50x.html {
+          root   /usr/share/nginx/html;
+      }
+
+      # proxy the PHP scripts to Apache listening on 127.0.0.1:80
+      #
+      #location ~ \.php$ {
+      #    proxy_pass   http://127.0.0.1;
+      #}
+
+      # pass the PHP scripts to FastCGI server listening on 127.0.0.1:9000
+      #
+      #location ~ \.php$ {
+      #    root           html;
+      #    fastcgi_pass   127.0.0.1:9000;
+      #    fastcgi_index  index.php;
+      #    fastcgi_param  SCRIPT_FILENAME  /scripts$fastcgi_script_name;
+      #    include        fastcgi_params;
+      #}
+
+      # deny access to .htaccess files, if Apache's document root
+      # concurs with nginx's one
+      #
+      #location ~ /\.ht {
+      #    deny  all;
+      #}
+  }
+FOE
+}
+
+deployContent ()
+{
+  mkdir /var/www/html
+  cd /var/www/html
+  git clone $cccGitHubRepoURL
+  # chmod 000 .git
+}
 
 case $1 in
   install)
@@ -90,9 +148,8 @@ case $1 in
     startNginx
     ;;
   deploy)
-    cd /var/www/html
-    git clone $cccGitHubRepoURL
-    # chmod 000 .git
+    configureNginx
+    deployContent
     ;;
   upgrade)
     stopNginx
